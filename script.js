2c3d1bb01f767d95d66cd289a54d60984436b370
Diégane Faye <106848507+diazfaye@users.noreply.github.com>
2024-03-26 12:34:45 +0000

    Update script.js
    Mise à jour du message d'accueil

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // TYPED
 var typed = new Typed('.typed', {
-    strings: ["Bienvenue sur mon <strong>portfolio</strong>, je me présente je m'appelle Diégane Faye","Je suis éléve ingénieur en transmission des données et sécurité de l'information(<strong>TDSI</strong>)","Après une formation m'ayant fait découvrir plusieurs domaines de l'informatique . J'ai décidé de me lancer à fond dans l'apprentissage du code, notamment aux langages <strong>Front</strong> tels que CSS, HTML, Javascript et aux langages <strong>back end</strong> afin de devenir développeur. Plus qu'un avenir professionnel, je me suis découvert une passion, celle de coder.J'aime travailler avec Odoo,Je suis également passionné par la cybersécurité et aussi par les nouvelles technologies."],
+    strings: ["Bienvenue sur mon <strong>portfolio</strong>, je me présente je m'appelle Diégane Faye. Je suis ingénieur en transmission des données et sécurité de l'information (<strong>TDSI</strong>). Après une formation m'ayant fait découvrir plusieurs domaines de l'informatique, j'ai décidé de me lancer à fond dans l'apprentissage du code, notamment aux langages <strong>Front</strong> tels que CSS, HTML, Javascript, React, react Native, Angular et aux langages <strong>back end</strong> comme java, Spring boot afin de devenir développeur. Plus qu'un avenir professionnel, je me suis découvert une passion, celle de coder. J'aime travailler avec Odoo. Je suis également passionné par la cybersécurité et aussi par les nouvelles technologies. J'ai une année d'expérience chez Neurotech SA, où j'ai travaillé sur le support numéro 1 pour Odoo et utilisé Talend pour l'intégration de données (Salesforce vers SAP ou vice versa)."],
     typeSpeed: 20,
     loop: false
   });
